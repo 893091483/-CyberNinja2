2f4af4d6af796cdbdf57711d67b84b1302718364
Kamrin21 <dujiaming0731@gmail.com>
2021-02-19 12:59:10 -0500

    alpha

---
+++ CbyerNinja2/Assets/script/EnemyBehavior.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    
+    public GameObject fKey;
+    public bool playerIsAttack;
+    public float delayTime = 1;
+    public bool isHit;
+    public GameObject player;
+   // GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        fKey.SetActive(false);
+        
+    }
+    private void OnTriggerEnter2D(Collider2D  collision)
+    {
+
+        
+        fKey.SetActive(true);
+        player.GetComponent<PlayerMovement>().canAttack = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        
+        fKey.SetActive(false);
+        player.GetComponent<PlayerMovement>().canAttack = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       // playerIsAttack= player.GetComponent<PlayerMovement>().IsAttack;
+        if (isHit == true)
+        {
+            
+            player.GetComponent<PlayerMovement>().canAttack = false;
+            GameObject.Destroy(fKey);
+
+           GameObject.Destroy(gameObject,.8f);
+        }
+
+      //  if (playerIsAttack == true)
+       // {
+      //      GameObject.Destroy(fKey);
+       // }
+    }
+
+    
+}
